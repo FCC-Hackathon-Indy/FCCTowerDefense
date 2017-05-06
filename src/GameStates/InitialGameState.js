76c1ed39193f52452cb83215e3b00371e3a22ed3
James Maupin <jmsMaupin1@yahoo.com>
2017-05-06 16:59:24 -0400

    Add the tilemap and make it display

--- src/GameStates/InitialGameState.js
+++ src/GameStates/InitialGameState.js
@@ -1,4 +1,14 @@
 export default class InitialGameState extends Phaser.State {
-	create() {}
+	create() {
+		this.setUpMap();
+	}
 	update() {}
+
+	setUpMap() {
+		this.map = this.add.tilemap('level1');
+		this.map.addTilesetImage('wood_tileset');
+
+		this.tileLayer = this.map.createLayer('tilelayer');
+		this.tileLayer.resizeWorld();
+	}
 }
